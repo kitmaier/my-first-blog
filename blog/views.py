faa3b8fd6509b3ae0e4d6ce5f4670a4c73ae8a72
Kit Maier <8maier@gmail.com>
2017-05-10 17:30:08 -0500

    adding mobile support to base template

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,40 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+# list of mobile User Agents
+mobile_uas = [
+	'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
+	'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
+	'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
+	'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
+	'newt','noki','oper','palm','pana','pant','phil','play','port','prox',
+	'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
+	'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
+	'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
+	'wapr','webc','winw','winw','xda','xda-'
+	]
+ 
+mobile_ua_hints = [ 'SymbianOS', 'Opera Mini', 'iPhone' ]
+
+def mobileBrowser(request):
+    ''' Super simple device detection, returns True for mobile devices '''
+    mobile_browser = False
+    ua = request.META['HTTP_USER_AGENT'].lower()[0:4]
+    if (ua in mobile_uas):
+        mobile_browser = True
+    else:
+        for hint in mobile_ua_hints:
+            if request.META['HTTP_USER_AGENT'].find(hint) > 0:
+                mobile_browser = True
+    return mobile_browser
 
 def post_list(request):
+    if mobileBrowser(request):
+        base_template = 'blog/m_base.html'
+    else:
+        base_template = 'blog/base.html'
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts, 'base_template': base_template})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
